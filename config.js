b6e4e2fda8ee296237d601c8b6797bdfc81023f4
andreybejarano <andreybejarano@gmail.com>
2017-09-28 10:10:33 -0300

    fix: deleted aws keys

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
-'use strict'
+'use strict';
 
 const config = {
-    aws: {
-        accessKey: process.env.AWS_ACCESS_KEY,
-        secretKey: process.env.AWS_SECRET_KEY
-    }
-}
+	aws: {
+		accessKey: process.env.AWS_ACCESS_KEY,
+		secretKey: process.env.AWS_SECRET_KEY
+	},
+	secret: process.env.PLATZIGRAM_SECRET || 'pl4tzi'
+};
 
 module.exports = config;
